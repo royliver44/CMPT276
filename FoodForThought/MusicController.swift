bd7aaf98f7be3e0e5a896c82c77d455d359f7a68
Tom <tomzhangmc@gmail.com>
2018-06-30 14:48:07 -0700

    second Music submission

--- FoodForThought/MusicController.swift
+++ FoodForThought/MusicController.swift
@@ -13,7 +13,9 @@ import AVFoundation
 class musicController: UIViewController {
     //set up audio player
     var audioPlayer = AVAudioPlayer()
-    
+    var audioList = ["Joshua", "The Truth That You Leave", "Kiss The Rain"]
+    var listLen = 3
+    var itr = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,7 +26,7 @@ class musicController: UIViewController {
             
             audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: "October", ofType: "mp3")!))
             audioPlayer.prepareToPlay()
-            
+            //set up backgroud music playing
             var audioSession = AVAudioSession.sharedInstance()
             do {
                 try audioSession.setCategory(AVAudioSessionCategoryPlayback)
@@ -44,20 +46,47 @@ class musicController: UIViewController {
     //Music functions
     @IBAction func play(_ sender: UIButton) {
         audioPlayer.play()
+        
     }
     
     @IBAction func pause(_ sender: UIButton) {
         if audioPlayer.isPlaying{
             audioPlayer.pause()
         }
-        
     }
     
     @IBAction func restart(_ sender: UIButton) {
         if audioPlayer.isPlaying{
             audioPlayer.currentTime = 0
             audioPlayer.play()
+            
+
+            
         }    }
+    
+    @IBAction func next(_ sender: UIButton){
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: audioList[(itr+1) % 3], ofType: "mp3")!))
+            itr += 1
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        }catch{
+            print(error)
+        }
+        
+    }
+    @IBAction func prev(_ sender: UIButton){
+        do{
+            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: audioList[(itr-1) % 3], ofType: "mp3")!))
+            itr -= 1
+            audioPlayer.prepareToPlay()
+            audioPlayer.play()
+        }catch{
+            print(error)
+        }
+        
+    }
+    
     
     
     //end Music functions
