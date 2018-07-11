f994de49f944ff20f4ee4c6bbfd5ac938cb33a69
Andy Adams <awadams@sfu.ca>
2018-07-11 14:29:04 -0700

    j2 complete, j1 begun
    - Notification scheduling set up for j2
    - Template views set up for j1
    - NOTE: Storyboard edited

--- FoodForThought/FirstViewController.swift
+++ FoodForThought/FirstViewController.swift
@@ -11,52 +11,20 @@ import UserNotifications
 
 class FirstViewController: UIViewController {
 
-    var identifier: String = "alarm"
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
-        
-//        //let center = UNUserNotificationCenter.current()
-//
-//        let content = UNMutableNotificationContent()
-//        content.title = "Title"
-//        content.body = "Body"
-//        content.sound = UNNotificationSound.default()
-//
-//        let gregorian = Calendar(identifier: .gregorian)
-//        let now = Date()
-//        var components = gregorian.dateComponents([.year, .month, .day, .hour, .minute, .second], from: now)
-//        components.timeZone = .current
-//        print(components)
-//
-//        // Change the time to 7:00:00 in your locale
-//
-//        components.hour = 7
-//        components.minute = 53
-//        components.second = 0
-//
-//        print(components)
-//
-//        let date = gregorian.date(from: components)!
-//        print(date)
-//
-//        let triggerDaily = Calendar.current.dateComponents([.hour,.minute,.second,], from: date)
-//        let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDaily, repeats: true)
-//
-//
-//        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
-//        print("INSIDE NOTIFICATION")
-//
-//        UNUserNotificationCenter.current().add(request, withCompletionHandler: {(error) in
-//            if let error = error {
-//                print("SOMETHING WENT WRONG")
-//            }
-//        })
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.tabBarController?.tabBar.isHidden = false
-
+        //        let center = UNUserNotificationCenter.current()
+//        center.getPendingNotificationRequests { (notifications) in
+//            print("Count: \(notifications.count)")
+//            for item in notifications {
+//                print(item.content)
+//            }
+//        }
     }
 
     override func didReceiveMemoryWarning() {
