aabcf7b59eb3d1e64d03592362b580dd96bd93b7
Rusty <rww6@sfu.ca>
2018-07-18 00:55:29 -0700

    Journal entry

--- FoodForThought/UpdateItemViewController.swift
+++ FoodForThought/UpdateItemViewController.swift
@@ -6,4 +6,70 @@
 //  Copyright © 2018 Jordan Ehrenholz. All rights reserved.
 //
 
+import UIKit
+import CoreData
 import Foundation
+
+class UpdateItemViewController: UIViewController, UITextViewDelegate {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var item: Item!
+    
+    @IBOutlet weak var entryText: UITextView!
+    
+    @IBAction func dismiss(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func updateAction(_ sender: Any) {
+        
+        guard let newEntry = entryText.text else  {
+            return
+        }
+        
+        item.name = newEntry
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        dismiss(animated: true, completion: nil)
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+        
+        entryText!.delegate = self
+        entryText!.becomeFirstResponder()
+        configureEntryData(entry: item)
+        
+        print(item)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func configureEntryData(entry: Item) {
+        
+        guard let text = entry.name else {
+            return
+        }
+        
+        entryText!.text = text
+    }
+    
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+    
+    
+}
--- FoodForThought/addItemViewController.swift
+++ FoodForThought/addItemViewController.swift
@@ -5,5 +5,70 @@
 //  Created by Russell Wong on 2018-07-17.
 //  Copyright © 2018 Jordan Ehrenholz. All rights reserved.
 //
-
+import UIKit
+import CoreData
 import Foundation
+
+
+class addItemViewController: UIViewController, UITextViewDelegate {
+    
+    @IBOutlet var itemEntryTextView: UITextView?
+    
+    @IBAction func cancel(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    @IBAction func saveContact(_ sender: Any) {
+        
+        if (itemEntryTextView?.text.isEmpty)! || itemEntryTextView?.text == "Type anything..."{
+            print("No Data")
+            
+            let alert = UIAlertController(title: "Please Type Something", message: "Your entry was left blank.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Okay", style: .default) { action in
+                
+            })
+            
+            self.present(alert, animated: true, completion: nil)
+            
+        } else {
+            
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            let newEntry = Item(context: context)
+            newEntry.name = itemEntryTextView?.text!
+            
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            
+            dismiss(animated: true, completion: nil)
+            
+        }
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        itemEntryTextView?.delegate = self
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        textView.text = ""
+        textView.textColor = UIColor.black
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if (text == "\n") {
+            textView.resignFirstResponder()
+            return false
+        }
+        return true
+    }
+}
+
+
