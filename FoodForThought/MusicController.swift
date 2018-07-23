350b3153bf77b6bc542d4571f26706000656731a
Tom <tomzhangmc@gmail.com>
2018-07-23 02:54:07 -0700

    spotify Featured Playlist integration

--- FoodForThought/MusicController.swift
+++ FoodForThought/MusicController.swift
@@ -14,9 +14,10 @@ import SafariServices
 class musicController: UIViewController , UITableViewDataSource, UITableViewDelegate,SPTAudioStreamingPlaybackDelegate, SPTAudioStreamingDelegate{
     //set up audio player
     var audioPlayer = AVAudioPlayer()
-    
+    var playerFlag = 0
+    var AudioListCount = 0  // count AudioPlayer Files
     //todo: change the list to automatic generation. (done)
-    
+
     var audioList = [[String]]()
     var itr = 0
     var timer = Timer()
@@ -37,19 +38,17 @@ class musicController: UIViewController , UITableViewDataSource, UITableViewDele
         return cell
     }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
-        
+
         playByIndex(index: indexPath.row)
     }
-    
-    //spotify integration
-    // Variables
+
+    //spotify integration Variables
     var auth = SPTAuth.defaultInstance()!
     var session:SPTSession!
-    
     // Initialzed in either updateAfterFirstLogin: (if first time login) or in viewDidLoad (when there is a check for a session object in User Defaults
     var player: SPTAudioStreamingController?
     var loginUrl: URL?
-    
+    // end spotify integration Variables
     @IBOutlet weak var loginButton: UIButton!
     
     @IBAction func loginBtnPressed(_ sender: Any) {
@@ -65,7 +64,7 @@ class musicController: UIViewController , UITableViewDataSource, UITableViewDele
         let clientID = "8ac1120b0a0d413eb1151ba09e64b7f1" // put your client ID here
         auth.redirectURL     = URL(string: redirectURL)
         auth.clientID        = clientID
-        auth.requestedScopes = [SPTAuthStreamingScope, SPTAuthPlaylistReadPrivateScope, SPTAuthPlaylistModifyPublicScope, SPTAuthPlaylistModifyPrivateScope]
+        auth.requestedScopes = [SPTAuthStreamingScope, SPTAuthPlaylistReadPrivateScope, SPTAuthPlaylistModifyPublicScope, SPTAuthPlaylistModifyPrivateScope, SPTAuthUserLibraryReadScope]
         loginUrl = auth.spotifyWebAuthenticationURL()
     }
     
@@ -75,12 +74,7 @@ class musicController: UIViewController , UITableViewDataSource, UITableViewDele
             self.player!.playbackDelegate = self
             self.player!.delegate = self
             try! player?.start(withClientId: auth.clientID)
-            
-            self.player!.login(withAccessToken: authSession.accessToken!)
-            self.player!.login(withAccessToken: authSession.accessToken!)
-            print(3)
-            print(authSession.accessToken!)
-            print(player!.loggedIn)
+            self.player!.login(withAccessToken: authSession.accessToken)
         }
         
     }
@@ -102,19 +96,47 @@ class musicController: UIViewController , UITableViewDataSource, UITableViewDele
         }
         
     }
+    func featuredPlaylist(itr: Int){
+        var flag = false
+        do{
+            
+            let featured_music = try SPTBrowse.createRequestForFeaturedPlaylists(inCountry: "CA", limit: 10, offset: 0, locale: nil, timestamp: nil, accessToken: session.accessToken)
+            let task2 = URLSession.shared.dataTask(with: featured_music, completionHandler: {(data,response,error) in
+                do{
+                    let featured_playlist = try SPTFeaturedPlaylistList.init(from: data, with: response)
+                    
+                    for i in 0...itr {
+                    let feat_playlist = featured_playlist.items[i] as! SPTPartialPlaylist
+                    // add label to audio list
+                    let label = "Featured \(self.audioList.count - self.AudioListCount + 1): \(feat_playlist.name!) (\(feat_playlist.trackCount) Tracks)"
+                    self.audioList.append([label,feat_playlist.playableUri.absoluteString])
+                    }
+                    flag = true
+                    
+                }catch{
+                    print("error in parsing data from Spotify")
+                }
+            })
+            task2.resume()
+        }catch{
+            print("error2")
+        }
+        
+        while !flag{}
+        Playlist.reloadData()
+        
+    }
     func audioStreamingDidLogin(_ audioStreaming: SPTAudioStreamingController!) {
         // after a user authenticates a session, the SPTAudioStreamingController is then initialized and this method called
         print("logged in")
-        self.player?.playSpotifyURI("spotify:track:1p80LdxRV74UKvL8gnD7ky", startingWith: 0, startingWithPosition: 0, callback: { (error) in
-            if (error != nil) {
-                print("playing!")
-            }
-            
-        })
+        if( audioPlayer.isPlaying){
+            audioPlayer.pause()
+        }
+        featuredPlaylist(itr: 5)
         
     }
     //
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -122,7 +144,7 @@ class musicController: UIViewController , UITableViewDataSource, UITableViewDele
         Playlist.delegate = self
         //Automatic Music list generation (mp3 only)
         let docsPath = Bundle.main.resourcePath!
-        
+
         let fileManager = FileManager.default
         do{
             //getting all files
@@ -140,13 +162,13 @@ class musicController: UIViewController , UITableViewDataSource, UITableViewDele
             }
             //adding to audioList
             for each in docsArray{
-                audioList.append(each.components(separatedBy: "."))
+                 audioList.append(each.components(separatedBy: "."))
             }
         }catch{
             print(error)
         }
         
-        
+        AudioListCount = audioList.count
         //Audio setup
         
         do{
@@ -181,27 +203,58 @@ class musicController: UIViewController , UITableViewDataSource, UITableViewDele
     
     //Music functions
     func playByIndex(index: Int){
-        do{
-            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: audioList[index][0], ofType: audioList[index][1])!))
-            itr = index
-            audioPlayer.prepareToPlay()
-            play()
-        }catch{
-            print(error)
+            //pause any playing music
+            if playerFlag == 0{
+                if audioPlayer.isPlaying{
+                    audioPlayer.pause()
+                }
+            }else{
+                if player?.playbackState != nil {
+                    if (player?.playbackState.isPlaying)! {
+                        player?.setIsPlaying(false, callback: nil)
+                    }
+                }
+            }
+         if(audioList.count - (index) > audioList.count - AudioListCount){
+            //if selected file is from AVAudioPlayer
+            do{
+
+                //play
+                playerFlag = 0
+                audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: audioList[index][0], ofType: audioList[index][1])!))
+                itr = index
+                audioPlayer.prepareToPlay()
+                play()
+            }catch{
+                print(error)
+            }
+        }
+        else{
+            playerFlag = 1
+            player?.playSpotifyURI(audioList[index][1], startingWith: 0, startingWithPosition: 0, callback: nil)
+            play() // for enitiating label update
         }
     }
     func play(){
-        audioPlayer.play()
         
-        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(lableUpdate), userInfo: nil, repeats: true)
-        
-        // todo: creat a thread that call next up on finish playing.(done)
-        DispatchQueue.global(qos: .background).async {
+        timer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(lableUpdate), userInfo: nil, repeats: true)
+        if playerFlag == 0 {
+            audioPlayer.play()
             
-            while(self.audioPlayer.duration - self.audioPlayer.currentTime > 1){
-                sleep(1)
+             // todo: creat a thread that call next up on finish playing.(done)
+            DispatchQueue.global(qos: .background).async {
+                
+                while(self.audioPlayer.duration - self.audioPlayer.currentTime > 1){
+                    sleep(1)
+                }
+                self.next()
             }
-            self.next()
+        }else{
+            if player?.playbackState != nil{
+                if player?.playbackState.isPlaying == false {
+                    player?.setIsPlaying(true, callback: nil)
+                }
+            }
         }
     }
     
@@ -211,71 +264,137 @@ class musicController: UIViewController , UITableViewDataSource, UITableViewDele
     @IBOutlet weak var musicCurrentDisplay: UILabel?
     
     @objc func lableUpdate(){
-        //Total duration of play.
-        if((Int(audioPlayer.duration) % 60) > 9){
-            musicTimeDisplay?.text = "\(Int(audioPlayer.duration) / 60):\(Int(audioPlayer.duration) % 60)"
-        }else{
-            musicTimeDisplay?.text = "\(Int(audioPlayer.duration) / 60):0\(Int(audioPlayer.duration) % 60)"
-        }
-        //currentTime of play
-        if((Int(audioPlayer.currentTime) % 60) > 9){
+        if playerFlag == 0 { //if AVAudio is playing
+            //duration label
+            if((Int(audioPlayer.duration) % 60) > 9){
+                musicTimeDisplay?.text = "\(Int(audioPlayer.duration) / 60):\(Int(audioPlayer.duration) % 60)"
+            }else{
+                musicTimeDisplay?.text = "\(Int(audioPlayer.duration) / 60):0\(Int(audioPlayer.duration) % 60)"
+            }
+            //currentTime label
+            if((Int(audioPlayer.currentTime) % 60) > 9){
             musicCurrentDisplay?.text = "\(Int(audioPlayer.currentTime) / 60):\(Int(audioPlayer.currentTime) % 60)"
+            }else{
+                musicCurrentDisplay?.text = "\(Int(audioPlayer.currentTime) / 60):0\(Int(audioPlayer.currentTime) % 60)"
+            }
+            //drag bar label
+            musicDragBar?.setValue(Float(audioPlayer.currentTime / audioPlayer.duration), animated: true)
         }else{
-            musicCurrentDisplay?.text = "\(Int(audioPlayer.currentTime) / 60):0\(Int(audioPlayer.currentTime) % 60)"
+            if (player?.playbackState.isPlaying)! {
+                if((Int((player?.metadata.currentTrack?.duration)!) % 60) > 9){
+                    musicTimeDisplay?.text = "\(Int((player?.metadata.currentTrack?.duration)!) / 60):\(Int((player?.metadata.currentTrack?.duration)!) % 60)"
+                }else{
+                    musicTimeDisplay?.text = "\(Int((player?.metadata.currentTrack?.duration)!) / 60):0\(Int((player?.metadata.currentTrack?.duration)!) % 60)"
+                }
+                //currentTime label
+                if((Int((player?.playbackState.position)!) % 60) > 9){
+                    musicCurrentDisplay?.text = "\(Int((player?.playbackState.position)!) / 60):\(Int((player?.playbackState.position)!) % 60)"
+                }else{
+                    musicCurrentDisplay?.text = "\(Int((player?.playbackState.position)!) / 60):0\(Int((player?.playbackState.position)!) % 60)"
+                }
+                //drag bar label
+                musicDragBar?.setValue(Float((player?.playbackState.position)! / (player?.metadata.currentTrack?.duration)!), animated: true)
+            }
         }
-        
-        musicDragBar?.setValue(Float(audioPlayer.currentTime / audioPlayer.duration), animated: true)
-        
     }
     
     func next(){
-        do{
-            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: audioList[(itr+1) % audioList.count][0], ofType: audioList[(itr+1) % audioList.count][1])!))
-            itr += 1
-            audioPlayer.prepareToPlay()
-            play()
-        }catch{
-            print(error)
+        if playerFlag == 0 {
+            do{
+                audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: audioList[(itr+1) % audioList.count][0], ofType: audioList[(itr+1) % audioList.count][1])!))
+                itr += 1
+                audioPlayer.prepareToPlay()
+                play()
+            }catch{
+                print(error)
+            }
+        }else{
+            player?.skipNext(nil)
         }
     }
     
     @IBAction func musicDrag(_ sender: UISlider) {
-        audioPlayer.currentTime = TimeInterval(Int((musicDragBar?.value)! * Float(audioPlayer.duration)))
+        if playerFlag == 0 {
+            audioPlayer.currentTime = TimeInterval(Int((musicDragBar?.value)! * Float(audioPlayer.duration)))
+        }else{
+            player?.seek(to: TimeInterval(Int((musicDragBar?.value)! * Float((player?.metadata.currentTrack?.duration)!))), callback: nil)
+        }
     }
     
     @IBAction func play(_ sender: UIButton) {
         play()
         
     }
-    
+
     @IBAction func pause(_ sender: UIButton) {
-        if audioPlayer.isPlaying{
-            audioPlayer.pause()
+        if playerFlag == 0{
+            if audioPlayer.isPlaying{
+                audioPlayer.pause()
+            }
+        }else{
+            if player?.playbackState != nil {
+                if (player?.playbackState.isPlaying)! {
+                    player?.setIsPlaying(false, callback: nil)
+                }
+            }
         }
+        
+        // test, delete after
+        /*
+        do{
+        let user_music = try SPTYourMusic.createRequestForCurrentUsersSavedTracks(withAccessToken: session.accessToken)
+        let task = URLSession.shared.dataTask(with: user_music, completionHandler: {(data,response,error) in
+            do{
+                let user_playlist = try SPTPlaylistList.init(from: data!, with: response)
+                //print(user_playlist.items)
+            }catch{
+                print("error in parsing data from Spotify")
+            }
+        })
+            
+        task.resume()
+ 
+        }catch {
+            print("error")
+        }
+           */
+        
     }
     
     @IBAction func restart(_ sender: UIButton) {
-        if audioPlayer.isPlaying{
-            audioPlayer.currentTime = 0
-            play()
+        if playerFlag == 0 {
+            if audioPlayer.isPlaying{
+                audioPlayer.currentTime = 0
+                play()
+            }
+        }else{
+            if player?.playbackState != nil {
+                if (player?.playbackState.isPlaying)! {
+                    player?.seek(to: 0, callback: nil)
+                }
+            }
         }
     }
     
     @IBAction func next(_ sender: UIButton){
-        next()
+            next()
     }
     @IBAction func prev(_ sender: UIButton){
-        do{
-            audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: audioList[(itr-1) % audioList.count][0], ofType: audioList[(itr-1) % audioList.count][1])!))
-            itr -= 1
-            audioPlayer.prepareToPlay()
-            play()
-        }catch{
-            print(error)
+        if playerFlag == 0 {
+            do{
+                audioPlayer = try AVAudioPlayer(contentsOf: URL.init(fileURLWithPath: Bundle.main.path(forResource: audioList[(itr-1) % audioList.count][0], ofType: audioList[(itr-1) % audioList.count][1])!))
+                itr -= 1
+                audioPlayer.prepareToPlay()
+                play()
+            }catch{
+                print(error)
+            }
+        }else{
+            //todo
+            player?.skipPrevious(nil)
         }
         
     }
     
     //end Music functions
 }
-
